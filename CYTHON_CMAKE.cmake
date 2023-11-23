bce240257888581b64be606a5058690277ced04d
Vyas Ramasubramani <vyasr@nvidia.com>
2023-11-23 14:44:17 -0800

    Add cmakelint

--- CYTHON_CMAKE.cmake
+++ CYTHON_CMAKE.cmake
@@ -1,7 +1,7 @@
 # This file provides a convenient entrypoint for using the CMake functions in
 # this package without installing the cython-cmake Python package.
 
-set(cython-cmake-url
+set(CYTHON_CMAKE_URL
     https://github.com/vyasr/cython-cmake/archive/refs/heads/main.zip)
 
 include(FetchContent)
@@ -10,7 +10,7 @@ if(POLICY CMP0135)
   cmake_policy(PUSH)
   cmake_policy(SET CMP0135 NEW)
 endif()
-FetchContent_Declare(cython-cmake URL "${cython-cmake-url}")
+FetchContent_Declare(cython-cmake URL "${CYTHON_CMAKE_URL}")
 if(POLICY CMP0135)
   cmake_policy(POP)
 endif()
@@ -18,10 +18,10 @@ endif()
 FetchContent_GetProperties(rapids-cmake)
 if(NOT cython-cmake_POPULATED)
   FetchContent_MakeAvailable(cython-cmake)
-  set(cython-cmake-dir "${cython-cmake_SOURCE_DIR}")
+  set(CYTHON_CMAKE_DIR "${cython-cmake_SOURCE_DIR}")
 endif()
 
 # Set up the CMAKE_MODULE_PATH
-if(NOT "${cython-cmake-dir}/cmake" IN_LIST CMAKE_MODULE_PATH)
-  list(APPEND CMAKE_MODULE_PATH "${cython-cmake-dir}/cmake")
+if(NOT "${CYTHON_CMAKE_DIR}/cmake" IN_LIST CMAKE_MODULE_PATH)
+  list(APPEND CMAKE_MODULE_PATH "${CYTHON_CMAKE_DIR}/cmake")
 endif()
--- cmake/Findcython.cmake
+++ cmake/Findcython.cmake
@@ -1,16 +1,61 @@
 include_guard(GLOBAL)
 
+#[=======================================================================[.rst:
+cython
+------
+
+Transpile a Cython file to C or C++.
+
+.. code-block:: cmake
+
+  cython(SOURCE_FILES <filenames...>
+         [TARGET_LANGUAGE (C|CXX)]
+         [LANGUAGE_LEVEL (2|3|3str)]
+         [CYTHON_ARGS <args...>])
+
+
+Generates a C or C++ file from a list of Cython files. The generated files
+are placed in the build tree at the same relative location as the source files.
+
+``SOURCE_FILES``
+  A list of Cython files to transpile.
+
+``TARGET_LANGUAGE``
+  The target language for the generated file. Must be one of ``C`` or ``CXX``.
+  Defaults to ``C``.
+
+``LANGUAGE_LEVEL``
+  The language level for the generated file. Must be one of ``2``, ``3``, or
+  ``3str``. Defaults to ``3str``.
+
+``CYTHON_ARGS``
+  A list of additional arguments to pass to Cython.
+
+Result Targets
+^^^^^^^^^^^^^^
+  A target is created for each Cython file in ``SOURCE_FILES``. The name of the
+  target is the name of the Cython file with the extension removed and
+  ``_transpiled`` appended. For example, ``foo.pyx`` would result in a target
+  named ``foo_transpiled``.
+
+Result Variables
+^^^^^^^^^^^^^^^^
+  :cmake:variable:`CYTHON_COMPILE_CREATED_FILES` is set to the absolute path to
+  the list of generated files.
+
+#]=======================================================================]
+# cython: Transpile a Cython file to C or C++.
 function(cython)
   list(APPEND CMAKE_MESSAGE_CONTEXT "cython")
 
   find_program(CYTHON "cython" REQUIRED)
 
-  set(_cython_options)
-  set(_cython_one_value TARGET_LANGUAGE LANGUAGE_LEVEL)
-  set(_cython_multi_value CYTHON_ARGS SOURCE_FILES)
+  set(cython_options)
+  set(cython_one_value TARGET_LANGUAGE LANGUAGE_LEVEL)
+  set(cython_multi_value CYTHON_ARGS SOURCE_FILES)
 
-  cmake_parse_arguments(_CYTHON "${_cython_options}" "${_cython_one_value}"
-                        "${_cython_multi_value}" ${ARGN})
+  cmake_parse_arguments(_CYTHON "${cython_options}" "${cython_one_value}"
+                        "${cython_multi_value}" ${ARGN})
 
   # Match the Cython default language level.
   set(language_level --3str)
@@ -34,7 +79,7 @@ function(cython)
   endif()
 
   # Maintain list of generated targets
-  set(CREATED_FILES "")
+  set(created_files "")
 
   foreach(cython_filename IN LISTS _CYTHON_SOURCE_FILES)
     cmake_path(GET cython_filename FILENAME cython_module)
@@ -66,13 +111,16 @@ function(cython)
 
     # Create a target that can be depended on by downstream targets to ensure
     # that the Cython file is compiled.
-    add_custom_target("${cython_module}_transpiled"
-                      DEPENDS "${full_transpiled_filename}")
-    list(APPEND CREATED_FILES
+    add_custom_target(
+      "${cython_module}_transpiled"
+      DEPENDS "${full_transpiled_filename}"
+      COMMENT
+        "Transpiling ${full_cython_filename} to ${full_transpiled_filename}")
+    list(APPEND created_files
          "${CMAKE_CURRENT_BINARY_DIR}/${transpiled_filename}")
   endforeach()
 
   set(CYTHON_COMPILE_CREATED_FILES
-      ${CREATED_FILES}
+      ${created_files}
       PARENT_SCOPE)
 endfunction()
